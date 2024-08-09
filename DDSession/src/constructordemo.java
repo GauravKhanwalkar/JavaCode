650f704768c708a42627b5f6aae77c5848e703a1
gaurav <gauravkhanwalkar1@gmail.com>
2024-08-09 22:58:30 +0530

    Assignment 3 constructor along with THIS keyword

--- DDSession/src/constructordemo.java
+++ DDSession/src/constructordemo.java
@@ -4,28 +4,28 @@ public class constructordemo {
 	
 	public  constructordemo() {
 		
-		this(11,96,78);
-		System.out.println("default constructor");
+		this(11,96,78); //Navigate towards the Three parameterized constructor
+		System.out.println("Default constructor");
 	}
 	
 	public constructordemo(int a) {
-		this(22,45);
-		System.out.println("one parameterized constructor");
+		this(22,45);//Navigate towards the Two parameterized constructor
+		System.out.println("One parameterized constructor");
 	}
 	
 	public constructordemo(int a,int b) {
 		
-		this();
-		System.out.println("two parameterized constructor");
+		this();//Navigate towards the default parameterized constructor
+		System.out.println("Two parameterized constructor");
 	}
 	
 	public constructordemo(int a,int b,int c) {
 		
-		System.out.println("three parameterized constructor");
+		System.out.println("Three parameterized constructor");
 	}
 	
 	public static void main(String[] args) {
-		constructordemo cd=new constructordemo(21);
+		constructordemo cd=new constructordemo(21);//Navigate towards the One parameterized constructor
 		
 	}
 	
